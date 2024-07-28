117daf01c90f6629ccf111b50f5f7285fe28da33
SalwaMahmoud9 <sma.mam9@gmail.com>
2024-07-28 12:06:54 +0200

    28-7-2024

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""utilities.
+"""Generic utilities for github org client.
 """
 import requests
 from functools import wraps
@@ -11,7 +11,6 @@ from typing import (
     Callable,
 )
 
-
 __all__ = [
     "access_nested_map",
     "get_json",
@@ -20,11 +19,18 @@ __all__ = [
 
 
 def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
-    """access_nested_map.
-    Para
+    """Access nested map with key path.
+    Parameters
     ----------
     nested_map: Mapping
+        A nested map
     path: Sequence
+        a sequence of key representing a path to the value
+    Example
+    -------
+    >>> nested_map = {"a": {"b": {"c": 1}}}
+    >>> access_nested_map(nested_map, ["a", "b", "c"])
+    1
     """
     for key in path:
         if not isinstance(nested_map, Mapping):
@@ -35,20 +41,33 @@ def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
 
 
 def get_json(url: str) -> Dict:
-    """get_json.
+    """Get JSON from remote URL.
     """
     response = requests.get(url)
     return response.json()
 
 
 def memoize(fn: Callable) -> Callable:
-    """memoize.
+    """Decorator to memoize a method.
+    Example
+    -------
+    class MyClass:
+        @memoize
+        def a_method(self):
+            print("a_method called")
+            return 42
+    >>> my_object = MyClass()
+    >>> my_object.a_method
+    a_method called
+    42
+    >>> my_object.a_method
+    42
     """
     attr_name = "_{}".format(fn.__name__)
 
     @wraps(fn)
     def memoized(self):
-        """"memoized"""
+        """"memoized wraps"""
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
